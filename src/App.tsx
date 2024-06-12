db9add3146807e3f4ad2eb6ba9ad591cc358c6f4
Vivek <vivekradhakrishna77@gmail.com>
2024-06-12 11:18:55 +0100

    add image and change css flex

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h2>My todos</h2>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
